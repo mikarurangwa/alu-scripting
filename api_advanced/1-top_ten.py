2240ce617f430dd0093c033c8f80df79176b5952
mikarurangwa <r.mika@alustudent.com>
2024-10-25 23:41:50 +0200

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 """
+1-top_ten.py
+
 This module queries the Reddit API to print the titles of the first
-10 hot posts for a given subreddit.
+10 hot posts for a given subreddit. If the subreddit is invalid or 
+there are no posts, it prints 'None'.
 """
 import requests
 
@@ -9,8 +12,7 @@ import requests
 def top_ten(subreddit):
     """
     Queries the Reddit API and prints the titles of the first 10 hot posts
-    for the given subreddit.
-    If the subreddit is invalid, it prints 'None'.
+    for the given subreddit. If the subreddit is invalid, it prints 'None'.
     """
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
     headers = {'User-Agent': 'Python/requests:API_advanced:v1.0 (by /u/your_reddit_username)'}
