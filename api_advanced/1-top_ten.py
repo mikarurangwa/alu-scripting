bb19da1a7aece91a8e62f387837582212d360dab
mikarurangwa <r.mika@alustudent.com>
2024-10-25 23:44:19 +0200

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,32 +1,20 @@
 #!/usr/bin/python3
-"""
-1-top_ten.py
-
-This module queries the Reddit API to print the titles of the first
-10 hot posts for a given subreddit. If the subreddit is invalid or 
-there are no posts, it prints 'None'.
-"""
+""""Doc"""
 import requests
 
 
 def top_ten(subreddit):
-    """
-    Queries the Reddit API and prints the titles of the first 10 hot posts
-    for the given subreddit. If the subreddit is invalid, it prints 'None'.
-    """
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    headers = {'User-Agent': 'Python/requests:API_advanced:v1.0 (by /u/your_reddit_username)'}
+    """"Doc"""
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10" \
+        .format(subreddit)
 
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            data = response.json().get('data', {}).get('children', [])
-            if not data:
-                print(None)
-            else:
-                for post in data[:10]:
-                    print(post['data']['title'])
-        else:
-            print(None)
-    except Exception:
+    res = requests.get(url,
+                       headers={
+                           'User-Agent': 'Mozilla/5.0'})
+
+    if res.status_code != 200:
         print(None)
+    else:
+        json_response = res.json()
+        posts = json_response.get('data').get('children')
+        [print(post.get('data').get('title')) for post in posts]
