af91642ffe3ee4f5843df98b4e488639864e209f
mikarurangwa <r.mika@alustudent.com>
2024-10-25 23:32:12 +0200

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,16 +1,34 @@
 #!/usr/bin/python3
-""" top_ten.py """
+"""Prints the title of the first 10 hot posts listed for a given subreddit"""
+
 import requests
 
 
 def top_ten(subreddit):
-    """ prints the titles of the first 10 hot posts listed in a subreddit """
-    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code != 200:
-        print(None)
-        return
-    posts = response.json()['data']['children']
-    for post in posts:
-        print(post['data']['title'])
+    """Main function to fetch and print titles of hot posts from a subreddit."""
+    URL = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    HEADERS = {"User-Agent": "Mozilla/5.0"}
+
+    try:
+        RESPONSE = requests.get(URL, headers=HEADERS, allow_redirects=False)
+        
+        # Check if the response status code is not 200
+        if RESPONSE.status_code != 200:
+            print(None)
+            return
+        
+        HOT_POSTS = RESPONSE.json().get("data", {}).get("children", [])
+        
+        # Check if there are no hot posts
+        if not HOT_POSTS:
+            print(None)
+            return
+        
+        # Print the titles of the first 10 hot posts
+        for post in HOT_POSTS:
+            print(post.get('data').get('title'))
+        
+        return "OK"  # Indicate successful retrieval
+
+    except requests.exceptions.RequestException:
+        print(None)  # Handle request exceptions
